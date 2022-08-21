3c0af88a44656224360b7f32f83abbfa65d48152
Eddy Boughioul <eddyboughioul@hotmail.fr>
2022-08-22 01:02:36 +0200

    build

--- src/contract.ts
+++ src/contract.ts
@@ -1,6 +1,5 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
-  Contract,
   OrderAdded,
   OrderAddedOrderInfoStruct,
   OrderCanceled,
@@ -8,24 +7,36 @@ import {
   RoleAdminChanged,
   RoleGranted,
   RoleRevoked,
-  Sold
-} from "../generated/Contract/Contract"
+  Sold,
+  OrderDex
+} from "../generated/OrderDex/OrderDex"
 import { Order, Counter, Pair, PairPrice } from "../generated/schema"
 
 export function handleOrderAdded(event: OrderAdded): void {
-  let entity = new Order(event.params.id.toHex());
-  // Entity fields can be set based on event parameters
-
   let orderInfo = event.params.orderInfo;
+
+  let entity = new Order(event.params.id.toHex());
+  entity.amountToBuy = orderInfo.amountToBuy;
+  entity.amountToBuyCompleted = orderInfo.amountToBuyCompleted;
+  entity.amountToSell = orderInfo.amountToSell;
+  entity.amountToSellCompleted = orderInfo.amountToSellCompleted;
+  entity.feeAmount = orderInfo.feeAmount;
+  entity.status = orderInfo.status;
+  entity.trader = orderInfo.trader;
+  entity.tokenToSell = orderInfo.tokenToSell;
+  entity.tokenToBuy = orderInfo.tokenToBuy;
+  entity.timestamp = orderInfo.timestamp;
+  entity.fromETH = orderInfo.fromETH;
+  entity.toETH = orderInfo.toETH;
+
   let price = orderInfo.amountToBuy.div(orderInfo.amountToSell);
   let priceInverted = orderInfo.amountToBuy.div(orderInfo.amountToSell);
-
 
   updateCount("0x0", entity.status);
   updateCount("seller_" + event.params.trader.toHex(), entity.status);
   updateCount("pair_" + orderInfo.tokenToSell.toHex() + "_" + orderInfo.tokenToBuy.toHex(), entity.status);
-  updateAmount(orderInfo.tokenToSell, orderInfo.tokenToBuy, price, orderInfo.amountToSell, false);
-  updateAmount(orderInfo.tokenToBuy, orderInfo.tokenToSell, priceInverted, orderInfo.amountToBuy, true);
+  updateAmount(orderInfo.tokenToSell, orderInfo.tokenToBuy, price, orderInfo.amountToSell, false, false);
+  updateAmount(orderInfo.tokenToBuy, orderInfo.tokenToSell, priceInverted, orderInfo.amountToBuy, true, false);
 
   // Entities can be written to the store with `.save()`
   entity.save();
@@ -59,7 +70,7 @@ function updateCount(id: string, status: i32): void {
 }
 
 
-function updateAmount(tokenToSell: Bytes, tokenToBuy: Bytes, price: BigInt, amount: BigInt, reverse: boolean): void {
+function updateAmount(tokenToSell: Bytes, tokenToBuy: Bytes, price: BigInt, amount: BigInt, reverse: boolean, substract: boolean): void {
 
   let id = "pair_" + tokenToSell.toHex() + "_" + tokenToBuy.toHex();
   let entity = Pair.load(id);
@@ -92,16 +103,69 @@ function updateAmount(tokenToSell: Bytes, tokenToBuy: Bytes, price: BigInt, amou
     entity.buy = buy;
   }
 
-  pp.amount = pp.amount.plus(amount);
+  if (substract) {
+    pp.amount = pp.amount.minus(amount);
+  } else {
+    pp.amount = pp.amount.plus(amount);
+  }
 
   entity.save();
   pp.save();
 }
 
 export function handleOrderCanceled(event: OrderCanceled): void {
+  let entity = Order.load(event.params.id.toHex());
+  if (entity == null) {
+    entity = new Order(event.params.id.toHex());
+  }
 
+  let price = entity.amountToBuy.div(entity.amountToSell);
+  let priceInverted = entity.amountToBuy.div(entity.amountToSell);
+  let amount = entity.amountToSell.minus(entity.amountToSellCompleted);
+  let amount2 = entity.amountToBuy.minus(entity.amountToBuyCompleted);
+
+  if (event.params.partiallySold) {
+    entity.status = 3;
+  } else {
+    entity.status = 1;
+  }
+
+  updateCount("seller_" + entity.trader.toHex(), entity.status);
+  updateCount("pair_" + entity.tokenToSell.toHex() + "_" + entity.tokenToBuy.toHex(), entity.status);
+
+  updateAmount(entity.tokenToSell, entity.tokenToBuy, price, amount, false, true);
+  updateAmount(entity.tokenToBuy, entity.tokenToSell, priceInverted, amount2, true, true);
+
+  entity.save();
 }
 
 export function handleSold(event: Sold): void {
+  let contract = OrderDex.bind(Address.fromString("0xB79aF1F3dD7e25Da902363ef5E220470b9288021"));
 
+  let entity = Order.load(event.params.orderIdA.toHex());
+  if (entity == null) {
+    entity = new Order(event.params.orderIdA.toHex());
+  }
+
+  let orderInfo = contract.orders(event.params.orderIdA);
+
+  let price = entity.amountToBuy.div(entity.amountToSell);
+  let priceInverted = entity.amountToBuy.div(entity.amountToSell);
+  let amount = event.params.amount;
+  let amount2 = orderInfo.getAmountToSellCompleted().minus(entity.amountToSellCompleted);
+
+  entity.amountToBuyCompleted = orderInfo.getAmountToBuyCompleted();
+  entity.amountToSellCompleted = orderInfo.getAmountToSellCompleted();
+
+  if (event.params.filled) {
+    entity.status = 2;
+    updateCount("seller_" + entity.trader.toHex(), entity.status);
+    updateCount("pair_" + entity.tokenToSell.toHex() + "_" + entity.tokenToBuy.toHex(), entity.status);
+  }
+
+
+  updateAmount(entity.tokenToSell, entity.tokenToBuy, price, amount, false, true);
+  updateAmount(entity.tokenToBuy, entity.tokenToSell, priceInverted, amount2, true, true);
+
+  entity.save();
 }
